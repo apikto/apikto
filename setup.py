e48977af202301b08957906161a62c55460f0a50
apikto <93084809+apikto@users.noreply.github.com>
2022-09-01 14:02:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi9.py",
+    version = "0.1",
+    py_modules = ['agi9'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
